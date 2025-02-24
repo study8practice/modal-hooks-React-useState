4ab4179390ccbedff9b53a884f456a61f9349532
study8practice <156129782+study8practice@users.noreply.github.com>
2025-02-24 11:51:09 -0300

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Modal from './Modal';
+import ButtonModal from './ButtonModal';
+
+const App = () => {
+  const [modal, setModal] = React.useState(false);
+
+  return (
+    <div>
+      <Modal modal={modal} setModal={setModal} />
+      <ButtonModal setModal={setModal} />
+    </div>
+  );
+};
+
+export default App;
